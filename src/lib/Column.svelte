3877984d4f5ba1c89a9d85da938aae55eb6fe731
Jan Andersson <jan.andersson@gmail.com>
2022-09-09 15:36:37 +0000

    Render empty space if no cards in column

--- src/lib/Column.svelte
+++ src/lib/Column.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import Card from "./Card.svelte";
   import type { Card as CardType } from "./Deck";
+  import Space from "./Space.svelte";
 
   export let stack: CardType[];
   export let onClick: (card: CardType) => void;
 </script>
 
 <div class="column">
+  {#if stack.length == 0}
+    <div class="card-wrapper">
+      <Space />
+    </div>
+  {/if}
   {#each stack as card, i}
     <div class="card-wrapper">
       <Card {card} onClick={i == stack.length - 1 ? onClick : null} />
