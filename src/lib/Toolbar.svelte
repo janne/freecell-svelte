d8bcf7661db90ae2db254fe584f0c7802eecae8d
Jan Andersson <jan.andersson@gmail.com>
2022-09-10 22:23:26 +0000

    But New game and Restart on the left

--- src/lib/Toolbar.svelte
+++ src/lib/Toolbar.svelte
@@ -9,10 +9,14 @@
 </script>
 
 <div class="toolbar">
-  <button on:click={newGame}>New game</button>
-  <button disabled={undoDisabled} on:click={undo}>Undo</button>
-  <button disabled={redoDisabled} on:click={redo}>Redo</button>
-  <button disabled={restartDisabled} on:click={restart}>Restart</button>
+  <span>
+    <button on:click={newGame}>New game</button>
+    <button disabled={restartDisabled} on:click={restart}>Restart</button>
+  </span>
+  <span>
+    <button disabled={undoDisabled} on:click={undo}>Undo</button>
+    <button disabled={redoDisabled} on:click={redo}>Redo</button>
+  </span>
 </div>
 
 <style lang="scss">
@@ -20,7 +24,7 @@
     display: flex;
     flex-direction: row;
     width: 100%;
-    justify-content: end;
+    justify-content: space-between;
 
     button:disabled {
       cursor: auto;
