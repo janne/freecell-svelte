45c7137b1c50f58bae81a6d6cd1ac3ab275668bf
Jan Andersson <jan.andersson@gmail.com>
2022-09-10 22:33:10 +0000

    Fix empty space render

--- src/lib/Column.svelte
+++ src/lib/Column.svelte
@@ -9,11 +9,6 @@
 </script>
 
 <div class="column">
-  {#if stack.length == 0}
-    <div class="card-wrapper">
-      <Space />
-    </div>
-  {/if}
   {#each stack as card, i}
     <div class="card-wrapper" style={`top: ${i * 5}vh; left: ${column * 9}vw;`}>
       <Card {card} onClick={(card) => onClick(card, stack.length - i)} />
