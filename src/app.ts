bdcfec7de05b123e879c65ca2fd1335074fa0ba4
Jan Andersson <jan.andersson@gmail.com>
2022-09-13 11:46:57 +0000

    Add web-manifest and service-worker

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import "./app.css";
 import Game from "./lib/components/Game.svelte";
 
+window.addEventListener("load", () => {
+  registerSW();
+});
+
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("service-worker.js");
+    } catch (e) {
+      console.log("Service worker registration failed");
+    }
+  }
+}
+
 const app = new Game({
   target: document.getElementById("app")!
 });
