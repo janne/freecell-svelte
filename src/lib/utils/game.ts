6bd3afd53dc6a88865b850881ea2ddcb4857632f
Jan Andersson <jan.andersson@gmail.com>
2022-11-16 23:53:45 +0000

    Revert "Fix max amount of movable cards"
    This reverts commit 1d19510247814b3294353aab926b8c7ad3beeab3.

--- src/lib/utils/game.ts
+++ src/lib/utils/game.ts
@@ -103,11 +103,10 @@ export function addCard(card: Card, game: Game): GameUpdater | null {
 }
 
 export function moveStack(card: Card, game: Game, count: number): GameUpdater | null {
-  const free_columns = game.tableau.filter((column) => column.length === 0).length;
-  const free_cells = game.freeCells.filter((cell) => cell === null).length;
-  const max_moved = (2 ^ free_columns) * (free_cells + 1);
+  const holes =
+    game.tableau.filter((column) => column.length === 0).length + game.freeCells.filter((cell) => cell === null).length;
 
-  if (count > max_moved) return null;
+  if (count > holes + 1) return null;
 
   const column = game.tableau.findIndex((column) => column.includes(card))!;
   const index = game.tableau[column].findIndex((c) => c == card);
@@ -131,8 +130,7 @@ export function moveStack(card: Card, game: Game, count: number): GameUpdater | 
     };
   }
 
-  const max_moved_to_column = (2 ^ (free_columns - 1)) * (free_cells + 1);
-  if (count > max_moved_to_column) return null;
+  if (count > holes) return null;
 
   const j = game.tableau.findIndex((column) => column.length === 0);
   if (j != -1) {
